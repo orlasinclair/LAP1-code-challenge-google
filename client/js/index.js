dd1e0db711295c1344549bb2ac92a46abbedcbbd
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-02 16:51:26 +0200

    updated  files

--- client/js/index.js
+++ client/js/index.js
@@ -1,1 +1,16 @@
-/* JavaScript to capture queries*/
+const searchButton = document.querySelector('form')
+
+searchButton.addEventListener('submit', getResults)
+
+function getResults(e){
+    e.preventDefault()
+    fetch('http://localhost:3000/results')
+    .then(function(res){
+        res.json().then(function(json){
+            window.location.href = 'results.html'
+            let result = document.querySelector('h1')
+            result.textContent=json
+            console.log(json)
+        })
+    })
+}
