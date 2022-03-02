e5c9daa6915675f1874cc541c5eeb1b92548acd9
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-02 15:29:46 +0200

    updated server files, created Class of results

---
+++ server/models/result.js
@@ -0,0 +1,30 @@
+let resultsData = require("../data.js");
+
+class Result {
+    //Create a Result class
+    constructor(data) {
+      this.id = data.id;
+      this.result = data.result;
+    }
+
+    static get all() {
+        //Method to retrieve the data for all results
+        const results = resultsData.map((result) => new Result(result));
+        return results;
+      }
+
+    static findById(id) {
+        //Finds a result by its ID
+        try {
+        const resultData = resultsData.find((result) => result.id === id); //Returns the data of a result by using its ID
+        const result = new Result(resultData); /*Creates a new result, maybe we'll not use it since we are giving 10 fixed results*/
+        return result;
+        } catch (e) {
+        throw new Error("This ID does not exist");
+        }
+    }
+
+
+};    
+
+module.exports = Result;
