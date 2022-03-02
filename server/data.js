cd2ca9b1f856af4d8181bf7016ebd7d57ad69d26
dimi-fn <dimitris_fanis@protonmail.com>
2022-03-02 13:44:10 +0200

    test data on data.js

--- server/data.js
+++ server/data.js
@@ -2,7 +2,16 @@
 
 const searchResults = [
 
-    
+    { id: 1, company: "result 1" },
+    { id: 2, company: "result 2" },
+    { id: 3, company: "result 3"},
+    { id: 4, company: "result 4"},
+    { id: 5, company: "result 5"},
+    { id: 6, company: "result 6"},
+    { id: 7, company: "result 7"},
+    { id: 8, company: "result 8"},
+    { id: 9, company: "result 9"},
+    { id: 10, company: "result 10"},
 ];
 
 module.exports.searchResults;
