b2ca449aac084785aa5d8ff1a293229ec4f94c4f
“orlasinclair” <orla.sinclair@btinternet.comgit config --global user.name orlasinclairssh-keygengit config --global user.email your@emailadd.comgit version ssh-keygencat ~/.ssh/id_rsa.pubcat ~/.ssh/setup.pubcat ~/.ssh/id_rsa/setup.pubssh-keygencat ~/.ssh/id_rsa.pubssh-keygencat ~/.ssh/id_rsa.pubcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls -lahtouch .bashrccd ~ls -lahexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.shcurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscd ~ls - lshls- lahls -lahcode -r .bash_historycode .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashexport NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completionnvm lsnvm install v12.19.0nvm alias default 12.19.0export NVM_DIR=/Users/orlasinclair/.nvm[ -s /nvm.sh ] && \. /nvm.sh  # This loads nvm[ -s /bash_completion ] && \. /bash_completion  # This loads nvm bash_completionnode -vnpm -vnvm lscode.code .nvm lscurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lscode -r .bashrcnvmnode -vdocker run --name repo alpine/git clone https://github.com/docker/getting-started.gitdocker cp repo:/git/getting-started/ .cd getting-started  docker build -t docker101tutorial . docker run -d -p 80:80 --name docker-tutorial docker101tutorialnvm lssource ~/.bashrccurl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bashnvm lsnvm install v12.19.0node -vnpm -vnpm install -g laughshshabrew install pyenvcode . source ~/.bashrcpyenv install 3.9.1pyenv global 3.9.1python --versionpython -m pyenv global 3.9.1pyenv install 3.9.1pyenv global 3.9.1python -versionpython --versionpip --versionpython python --versiondocker run hello-worlddocker run hello-worlddocker run hello-worldgit clone git@github.com:orlasinclair/fp_study_notes_hello_github.gitcd fp_study_notes_hello_githubgit remote add upstream https://github.com/getfutureproof/fp_study_notes_hello_github.gitgit remote -vcode .git statusgit add .git statusgit commit -m add>
2022-03-04 09:43:28 +0000

    put both pages in same html so we can have one javascript file

--- client/index.js
+++ client/index.js
@@ -1,14 +1,37 @@
 const searchButton = document.getElementById('submit-btn')
+const homepage =document.getElementById('homepage')
+const resultPage = document.getElementById('results-page')
 
-searchButton.addEventListener('submit', redirect)
 
-function redirect(e){
-        e.preventDefault()
-            location.href = 'results.html'
-            console.log('hello')
+searchButton.addEventListener('submit', getResults)
 
-        }
-
+function getResults(e){
+    e.preventDefault()
+    if(resultPage.classList.contains('invisible')){
+        resultPage.classList.remove('invisible')
+        homepage.classList.add('invisible')
+    }
+    fetch('http://localhost:3000/results')
+    .then(function(res){
+        res.json().then(function(json){
+            for(let r of json){
+            let list = document.createElement('li')
+            let link=document.createElement('a')
+            let para = document.createElement('p')
+            let result = r.result
+            let resulttext= document.createTextNode(`${r.title}`)
+            let description = document.createTextNode(`${r.description}`)
+            para.appendChild(description)
+            link.href= result
+            link.appendChild(resulttext)
+            list.appendChild(link)
+            list.appendChild(para)
+            console.log(result)
+            let resultlist = document.querySelector('ul')
+            resultlist.append(list)
+        }})
+})
+}
 
 const randomButton = document.getElementById('random-btn')
 
@@ -25,5 +48,15 @@ function randomRedirect(e){
             console.log("hello")
         })
     })
+}
+
+let logoHome = document.getElementById('logo-result')
+logoHome.addEventListener('click', returnHome)
+
+function returnHome(){
+    if (homepage.classList.contains('invisible')){
+        homepage.classList.remove('invisible')
+        resultPage.classList.add('invisible')
+    }
 }
 
